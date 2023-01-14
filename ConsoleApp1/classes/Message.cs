b696b6ee857e2ff384dcaa43dd9531ac77c9e00e
engalimortada <ali.mortada@yandex.com>
2023-01-14 17:25:51 +0200

    init

---
+++ ConsoleApp1/classes/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.classes
+{
+    public static class Message
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("hi there");
+        }
+    }
+}
